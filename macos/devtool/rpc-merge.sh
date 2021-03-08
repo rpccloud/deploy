d018eadaef82a48dbc7e9ae217202f5f7d94e133
tslearn <tslearn@163.com>
2021-03-08 15:58:49 +0800

    Create rpc-merge.sh

---
+++ macos/devtool/rpc-merge.sh
@@ -0,0 +1,12 @@
+
+mkdir ~/tmp_rpc_merge && cd ~/tmp_rpc_merge
+
+git clone https://github.com/rpccloud/rpc
+cd cd ~/tmp_rpc_merge/rpc
+git checkout dev
+git checkout master
+git merge dev
+git push
+
+cd ~
+rm -rf ~/tmp_rpc_merge
